3144fb115c098d3176c7efa1a9e235f21485d3a8
Shuvankor-Hazra <shuvo.s.ovuhs@gmail.com>
2023-02-18 13:10:13 +0600

    404 page added

--- spotographer/src/App.js
+++ spotographer/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About/About";
 import Blog from "./components/Blog/Blog";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
